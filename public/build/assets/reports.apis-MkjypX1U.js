eeb075c329e1af71cbab96a27f5cdb6dbc80617f
hatem mazid <60873624+hatem-mazid@users.noreply.github.com>
2026-05-22 00:31:03 +0300

    npm run build

---
+++ public/build/assets/reports.apis-MkjypX1U.js
@@ -0,0 +1,1 @@
+import{h as r}from"./app-8g8ZBoyV.js";const s={getOrdersReport:async e=>r.get("/api/reports/orders",{params:e}),getExpensesReport:async e=>r.get("/api/reports/expenses",{params:e})};export{s as r};
